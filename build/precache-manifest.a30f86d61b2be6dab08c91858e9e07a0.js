1a31bc71c1742af32b11450ae97e06de1127ea9f
superweak <araell@outlook.com>
2025-08-02 22:33:01 +0800

    build

---
+++ build/precache-manifest.a30f86d61b2be6dab08c91858e9e07a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697c0b46f2d845b5358ababc4f197706",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "19dd9a2e6536b4d07b786548d9396094",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "838e0e853c0304c48a32a62a91a2aab5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04a50c1bab75f5702787af1a127087e2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "acd54e998b38cb482f69",
+    "url": "/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "acd54e998b38cb482f69",
+    "url": "/static/js/main.acd54e99.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "ded87515c392c3502f6c",
+    "url": "/static/js/vendors~main.ded87515.chunk.js"
+  }
+]);
